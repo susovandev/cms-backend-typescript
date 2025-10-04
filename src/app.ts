d2916ab6216f4cc0141cca61a634b685e6670586
Susovan Dev <susovandas.dev@gmail.com>
2025-10-04 12:36:33 +0530

    chore(server): add route notfound handler

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,10 @@
-import express, { Application } from 'express';
+import express, {
+    type Application,
+    Request,
+    Response,
+    NextFunction,
+} from 'express';
+import { StatusCodes } from 'http-status-codes';
 import { config } from './config/env-config.js';
 
 export class App {
@@ -8,7 +14,23 @@ export class App {
     }
 
     public start() {
+        this.setUpRoutes();
+        this.setupGlobalErrors();
         this.serverListen();
+    }
+
+    private setupGlobalErrors() {
+        this.app.use((req: Request, res: Response, next: NextFunction) => {
+            res.status(StatusCodes.NOT_FOUND).json({
+                status: false,
+                statusCode: StatusCodes.NOT_FOUND,
+                message:
+                    config.SERVER.NODE_ENV === 'production'
+                        ? 'No route found. Please contact the administrator.'
+                        : `Cant find this ${req.originalUrl} route. Please check the route again.`,
+            });
+            next();
+        });
     }
     private serverListen() {
         this.app.listen(config.SERVER.PORT, () => {
